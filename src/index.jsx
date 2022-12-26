b3a9c3345c3e75ca86958492600435f4b38d7868
Nisreen Hamzah <nisreenhamzah138@gmail.com>
2022-12-26 14:45:12 +0200

    added fontawesome

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import { store } from './app/store';
 import App from './App';
 import './index.css';
 
-// Bootstrap CSS
+// // Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Bootstrap Bundle JS
 import 'bootstrap/dist/js/bootstrap.bundle.min';
