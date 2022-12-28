2fe7c46140b30f763a50986e8ea72939e0408c83
theMstafa <mm22180481@gmail.com>
2022-12-28 22:09:44 +0300

    footer

--- src/components/Home/Home/HomeSection.js
+++ src/components/Home/Home/HomeSection.js
@@ -1,7 +1,7 @@
 import Header from '../Header/Header';
 import About from '../AboutSection';
 import Story from '../Story/Story';
-
+import Footer from '../Footer/Footer'
 import React from 'react';
 
 export default function HomeSection() {
@@ -10,6 +10,7 @@ export default function HomeSection() {
       <Header />
       <About />
       <Story />
+      <Footer/>
     </>
   );
 }
