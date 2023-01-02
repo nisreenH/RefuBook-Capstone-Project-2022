eb65ea12d0e06d54e851d6d86e9f45a917af929b
alimajid <alidzk98@gmail.com>
2023-01-02 16:15:24 +0300

    implementing facebook button for the signup page Fixes #19

--- src/components/Signup/buttons/Facebook.js
+++ src/components/Signup/buttons/Facebook.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { GrFacebookOption } from 'react-icons/gr';
+import { signInWithPopup, FacebookAuthProvider } from 'firebase/auth';
+import { authentication } from '../../../firebase';
 
 export default function Facebook() {
+  const signUpWithFacebook = () => {
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(authentication, provider)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
   return (
-    <button className="bg-facebookcolor text-white h-16 w-52 flex items-center justify-center rounded-full">
+    <button
+      onClick={signUpWithFacebook}
+      className="bg-facebookcolor text-white h-16 w-52 flex items-center justify-center rounded-full"
+    >
       <GrFacebookOption className="scale-150 text-2xl" />
     </button>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8YsobsXzBQWo7-yYNc0bEPHI36PR-TGY",
-  authDomain: "refubook-a51b5.firebaseapp.com",
-  projectId: "refubook-a51b5",
-  storageBucket: "refubook-a51b5.appspot.com",
-  messagingSenderId: "449333185859",
-  appId: "1:449333185859:web:3c122d056e54ce146e33f6",
-  measurementId: "G-1LW0ZVM399"
+  apiKey: 'AIzaSyD8YsobsXzBQWo7-yYNc0bEPHI36PR-TGY',
+  authDomain: 'refubook-a51b5.firebaseapp.com',
+  projectId: 'refubook-a51b5',
+  storageBucket: 'refubook-a51b5.appspot.com',
+  messagingSenderId: '449333185859',
+  appId: '1:449333185859:web:3c122d056e54ce146e33f6',
+  measurementId: 'G-1LW0ZVM399',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+const authentication = getAuth(app);
 
-export default db;
+export { db, authentication };
