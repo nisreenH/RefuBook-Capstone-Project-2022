be3f018191e4d47f7c101c1bd45604836c880f53
mahnaz nazir <mahnaz.n.ibrahim@gmail.com>
2023-01-03 19:12:44 +0300

    finished testimonial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import About from './components/Home/AboutSection';
+import Testimonials from './components/Home/Mahnaz/Testimonials';
 
 function App() {
   return (
     <div className="App">
       <header>this is the header</header>
       <About />
+      <Testimonials/>
     </div>
   );
 }
