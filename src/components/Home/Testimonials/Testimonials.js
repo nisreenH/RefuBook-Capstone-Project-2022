5633e28c76fa120dc7a615926fe7da43fe56b906
theMstafa <mm22180481@gmail.com>
2023-01-16 18:27:18 +0300

    all about page and testimonial and partners

--- src/components/Home/Testimonials/Testimonials.js
+++ src/components/Home/Testimonials/Testimonials.js
@@ -21,45 +21,44 @@ const testimonialsCarouselSettings = {
 function Testimonials() {
   const { t } = useTranslation();
   return (
-    <div className="my-8">
-      <h2 className="capitalize text-center text-blue-700 font-bold text-3xl xl:pt-20 sm:pt-10">
-        {t('home.testimonials.global-texts.testimonial-title')}
-      </h2>
+    <div className="my-8 ">
+      <div className='bg-prim '>
       <div className="flex flex-col xl:flex-row justify-evenly items-center py-8 lg:py-24">
         <div className="pb-8 lg:pb-0 flex justify-center items-center">
           <img className="w-1/2 mb-8 xl:mb-0" alt="world Map" src={WorldMap} />
         </div>
-        <div className="flex-end text-center w-full xl:w-2/4">
+        <div className="flex-end text-left w-full xl:w-2/4">
           <Slider {...testimonialsCarouselSettings}>
             <div>
-              <h3 className="text-gray-600 xl:w-1/2 text-center mx-auto">
+              <h3 className="text-gray-100 xl:w-1/2 text-left mx-auto ">
                 {t('home.testimonials.first.text')}
               </h3>
-              <p className="text-gray-600 pt-2">
+              <p className="text-whity text-left ml-44 pt-2">
                 <strong>{t('home.testimonials.first.whoSay')}</strong>
                 <br></br> {t('home.testimonials.first.position')}
               </p>
             </div>
             <div>
-              <h3 className="text-gray-600 xl:w-1/2 text-center mx-auto">
-                {t('home.testimonials.second.text')}
+              <h3 className="text-whity xl:w-1/2 text-left mx-auto">
+                {t('home.testimonials.first.text')}
               </h3>
-              <p className="text-gray-600 pt-2">
+              <p className="text-whity text-left ml-44 pt-2">
                 <strong>{t('home.testimonials.second.whoSay')}</strong>
-                <br></br> {t('home.testimonials.second.position')}
+                <br></br> {t('home.testimonials.first.position')}
               </p>
             </div>
             <div>
-              <h3 className="text-gray-600 xl:w-1/2 text-center mx-auto">
-                {t('home.testimonials.second.text')}
+              <h3 className="text-whity xl:w-1/2 text-left mx-auto">
+                {t('home.testimonials.first.text')}
               </h3>
-              <p className="text-gray-600 pt-2">
-                <strong>{t('home.testimonials.second.whoSay')}</strong>
-                <br></br> {t('home.testimonials.second.position')}
+              <p className="text-whity text-left ml-44 pt-2">
+                <strong>{t('home.testimonials.first.whoSay')}</strong>
+                <br></br> {t('home.testimonials.first.position')}
               </p>
             </div>
           </Slider>
         </div>
+      </div>
       </div>
       <div className="grid grid-cols-1 ">
         <div className="pt-16 pl-18 pb-20 sm:pl-10 lg:pl-34">
