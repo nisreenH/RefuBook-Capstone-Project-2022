f24d23c3a11752c4903fc7acbfcd63806feb2073
Ali Majed <alidzk98@gmail.com>
2023-01-23 19:31:58 +0300

    Home page reviewing Fixes #72

--- src/components/Home/Story/Story.js
+++ src/components/Home/Story/Story.js
@@ -1,103 +1,51 @@
-import React from 'react';
-// import pic from './image.jpg';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useState, useEffect } from 'react';
-import { onSnapshot, collection } from 'firebase/firestore';
+import StoryCard from './StoryCard';
 import { db } from '../../../firebase';
-
-function Story(props) {
-  const [blogs, setBlogs] = useState(null);
-  useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, 'blogs'), (snapshot) => {
-      setBlogs(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    });
-
-    return unsubscribe;
-  }, []);
-  // const {label} = props;
+import { collection, getDocs } from 'firebase/firestore';
+import Spinner from '../../spinner/Spinner';
+import shuffle from '../../../shuffle';
+function Story() {
   const { t } = useTranslation();
+
+  const [blogs, setBlogs] = useState({});
+  useEffect(() => {
+    async function fetchBlogs() {
+      const querySnapshot = await getDocs(collection(db, 'blogs'));
+      querySnapshot.forEach((doc) => {
+        setBlogs((prevState) => ({
+          ...prevState,
+          [doc.id]: doc.data(),
+        }));
+      });
+    }
+    fetchBlogs();
+  }, []);
+
+  let keys = Object.keys(blogs);
+  const indexes = [...Array(keys.length).keys()];
+  shuffle(indexes);
+  const randomIndex1 = indexes[0];
+  const randomKey1 = keys[randomIndex1];
+  const randomIndex2 = indexes[1];
+  const randomKey2 = keys[randomIndex2];
+
   return (
     <div className="bg-prim px-20">
-      <header className="text-white flex flex-col text-left gap-2 items-start pt-14 w-auto sans-Roboto ">
-        <h1 className="text-4xl font-normal sans-Roboto md:text-5xl">
+      <header className="text-white items-center text-justify pt-14 w-96 ">
+        <h1 className="text-4xl font-normal sans-Roboto leading-10 pb-2">
           {t('home.stories-section.title')}
         </h1>
-        <p className="bg-red-300 text-md md:text-lg lg:text-xl">
-          {t('home.stories-section.subtitle')}
-        </p>
+        <p className="">{t('home.stories-section.subtitle')} </p>
       </header>
-
-      {blogs
-        ? blogs.slice(0, 2).map((ele) => {
-            return (
-              <div>
-                <div>
-                  <img src={ele.blogImgUrl} alt={ele.title} />
-                </div>
-                <div>
-                  <label>{ele.categories}</label>
-                  <p>{ele.subTitle}</p>
-                  <div>
-                    <div>
-                      <div></div>
-                      <p>user name</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })
-        : ''}
-
-      {/* <div className="md:flex items-center p-12 gap-14 text-left text-white">
-        <div className="w-full">
-          <img className="shrink-0 sm:h-full" src={pic} alt="pic" />
-        </div>
-        <div className=" flex w-full relative p-16 flex-col gap-6">
-          <span className="font-bold my-7 bg-amber-200 text-prim text-xs p-2 rounded-md border-solid absolute -top-5 left-10">
-            {t('home.stories-section.details.language')}
-          </span>
-          <p className="w-70 text-2xl">
-            {t('home.stories-section.details.language-article-title')}
-          </p>
-          <div className="flex gap-x-8">
-            <img className="rounded-full w-14 h-14" src={pic} alt="pic" />
-            <div className="text-left">
-              <p className="font-bold text-sm ">
-                {t('home.stories-section.details.language-article-author')}
-              </p>
-              <span className="font-light text-xs">
-                {t('home.stories-section.details.language-article-state')}
-              </span>
-            </div>
-          </div>
-        </div>
-      </div> */}
-
-      {/* <div className="md:flex items-center p-12 gap-14 text-left text-white">
-        <div className="w-full">
-          <img className="shrink-0 sm:h-full" src={pic} alt="pic" />
-        </div>
-        <div className=" flex w-full relative p-16 flex-col gap-6">
-          <span className="font-bold my-7 bg-amber-200 text-prim text-xs p-2 rounded-md border-solid absolute -top-5 left-10">
-            {t('home.stories-section.details.job')}
-          </span>
-          <p className="w-70 text-2xl">
-            {t('home.stories-section.details.job-article-title')}
-          </p>
-          <div className="flex gap-x-8">
-            <img className="rounded-full w-14 h-14" src={pic} alt="pic" />
-            <div className="text-left">
-              <p className="font-bold text-sm ">
-                {t('home.stories-section.details.job-article-author')}
-              </p>
-              <span className="font-light text-xs">
-                {t('home.stories-section.details.job-article-state')}
-              </span>
-            </div>
-          </div>
-        </div>
-      </div> */}
+      {blogs[randomKey1] ? (
+        <>
+          <StoryCard props={blogs[randomKey1]} blogId={randomKey1} />
+          <StoryCard props={blogs[randomKey2]} blogId={randomKey2} />
+        </>
+      ) : (
+        <Spinner />
+      )}
     </div>
   );
 }
