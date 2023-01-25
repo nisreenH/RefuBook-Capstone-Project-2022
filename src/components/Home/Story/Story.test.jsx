21884eeff2d19da28547d111155fd56088af1ebe
mahnaz nazir <mahnaz.n.ibrahim@gmail.com>
2023-01-26 00:46:40 +0300

    finished test

---
+++ src/components/Home/Story/Story.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Story from './Story';
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Story></Story>, div);
+});
