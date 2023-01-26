e7ac7616049783d10ad3d98016f0c0f58719d479
theMstafa <mm22180481@gmail.com>
2023-01-26 06:29:47 +0300

    fix testing

--- src/components/teamSection/index.test.js
+++ src/components/teamSection/index.test.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import TeamSection from './TeamSection';
+import TeamSection from './index';
+import { render } from '@testing-library/react';
+import 'i18next/dist/cjs/i18next';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../../i18n';
 
-describe('TeamSection', () => {
-  it('renders correctly', () => {
-    render(
-      <I18nextProvider i18n={i18n}>
-        <TeamSection />
-      </I18nextProvider>
-    );
-
-    expect(screen.getByText('Team Heading')).toBeInTheDocument();
-    expect(screen.getByText('Country1')).toBeInTheDocument();
-    expect(screen.getByText('Country2')).toBeInTheDocument();
-    expect(screen.getByText('Country3')).toBeInTheDocument();
-    expect(screen.getByText('Country4')).toBeInTheDocument();
-    expect(screen.getByText('Team Member 1')).toBeInTheDocument();
-    expect(screen.getByText('Team Member 2')).toBeInTheDocument();
-    //...and so on for all the team members' names
-  });
+test('TeamSection renders correctly', () => {
+  const { container } = render(
+    <I18nextProvider i18n={i18n}>
+      <TeamSection />
+    </I18nextProvider>
+  );
+  expect(container).toMatchSnapshot();
 });
